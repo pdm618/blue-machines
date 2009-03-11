7c056e5f3c7e47e89a21ff25101962f84187b042
M.Glubokiy <M.Glubokiy@4a4d2d20-ed3a-11dd-a738-5fe2e3cfbcc5>
2009-03-11 05:40:22 +0000

    MINOR CHANGE: Som progress here

--- sw/MEngine/makebin.mk
+++ sw/MEngine/makebin.mk
@@ -1,32 +1,39 @@
-SOURCE  = $(wildcard *.c)
-OBJS    = $(STARTUPOBJ)
-OBJS    += $(patsubst %.c,%.o,$(SOURCE))
+include $(MENGINE)/env.mk
+
+# Sources
+CSOURCE   = $(patsubst ./%, %, $(shell find . -name '*.c'))
+ASOURCE   = $(patsubst ./%, %, $(shell find . -name '*.S'))
+aSOURCE   = $(patsubst ./%, %, $(shell find . -name '*.s'))
+
+AOBJS     = $(patsubst %.S, %.o,$(ASOURCE))
+AOBJS    += $(patsubst %.s, %.o,$(aSOURCE))
+COBJS     = $(patsubst %.c, %.o,$(CSOURCE))
+OBJS      = $(AOBJS)
+OBJS     += $(COBJS)
 
 PROGNAME        ?= noname
 PROGFILE        ?= $(PROGNAME).elf
 DUMPFILE        ?= $(PROGNAME).dump
 FIRMWAREFILE    ?= $(PROGNAME).fw
 
-LIBDEPS         = $(patsubst -l%,$(LIB)/lib%.a,$(LIBS))
+LDFLAGS     += $(patsubst -l%,lib%.a,$(LIBFILES))
+
 .PHONY: $(TARGETS)
 
 all: message $(FIRMWAREFILE)
-$(PROGFILE): $(OBJS) $(LIBDEPS)
+$(FIRMWAREFILE): $(PROGFILE)
+	$(RM) $(FIRMWAREFILE)
+	$(ECHO) "   FW .. $(patsubst $(ROOT)/%,%,$(FIRMWAREFILE))"
+	$(OBJCOPY) -O binary $(PROGFILE) $(FIRMWAREFILE)
+$(PROGFILE): $(OBJS) $(LIBS)
 	$(ECHO) "   -------------------- Linking to $(LSCRIPT) ---------------------------------"
 	$(ECHO) "   LD .. $(patsubst $(ROOT)/%,%,$(PROGFILE)) <- $(patsubst $(ROOT)/%.o,%.o,$(OBJS))  $(patsubst -l%,lib%.a, $(ALLLIBS))"
 	$(LD) -o $(PROGFILE) $(OBJS) $(LDFLAGS)
 	$(ECHO) "   ----------------------------------------------------------------------------"
 	$(ECHO) "   Executable created successfully. Build No: $(BUILD_NUMBER)"
 	$(ECHO) "   ----------------------------------------------------------------------------"
-	$(BUILD_NO)
 	$(ECHO) "   OD .. $(patsubst $(ROOT)/%,%,$(DUMPFILE))"
 	$(OBJDUMP) -SD $(PROGFILE) > $(DUMPFILE)
-$(FIRMWAREFILE): $(PROGFILE)
-	$(RM) $(FIRMWAREFILE) $(EXTFIRMWAREFILE)
-	$(ECHO) "   FW .. $(patsubst $(ROOT)/%,%,$(FIRMWAREFILE))"
-	$(OBJCOPY) -O binary $(PROGFILE) $(FIRMWAREFILE)
-	$(ECHO) "   CP .. $(patsubst $(ROOT)/%,%,$(FIRMWAREFILE)) -> $(FIRMWAREDIR)"
-	$(CP) $(FIRMWAREFILE) $(FIRMWAREDIR)
 %.o: %.c
 	$(ECHO) "   CC .. $<"
 	$(CC) $(CFLAGS) $< -o $@
@@ -34,5 +41,4 @@ message:
 	$(ECHO) "  Making executable [ $(PROGNAME) ] in [ $(patsubst $(ROOT)%,.%, $(shell pwd)) ]"
 clean:
 	$(ECHO) "  Cleaning executable [ $(PROGNAME) ] in [ $(patsubst $(ROOT)%,.%, $(shell pwd)) ]"
-	$(RM) $(PROGFILE) $(DUMPFILE) $(FIRMWAREFILE) $(EXTFIRMWAREFILE) $(OBJS)
-test:
+	$(RM) $(PROGFILE) $(DUMPFILE) $(FIRMWAREFILE) $(OBJS)
